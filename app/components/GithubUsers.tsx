4cd4191b1eddd64c8ab8f6318b7ff4288d8c94ce
Codey Gallup <codey.gallup@gmail.com>
2025-02-25 13:37:30 -0700

    Adding component for Github users

---
+++ app/components/GithubUsers.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  AppBar,
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+// import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+const users = [{id: 1, name: "John Doe", jiraTicket: "JIRA-1234"}, {id: 2, name: "Jane Doe", jiraTicket: "JIRA-1235"}, {id: 3, name: "John Smith", jiraTicket: "JIRA-1236"}, {id: 4, name: "Jane Smith", jiraTicket: "JIRA-1237"}, {id: 5, name: "John Doe", jiraTicket: "JIRA-1238"}, {id: 6, name: "Jane Doe", jiraTicket: "JIRA-1239"}, {id: 7, name: "Jane Smith", jiraTicket: "JIRA-1240"}, {id: 8, name: "John Smith", jiraTicket: "JIRA-1241"}];
+
+const GithubUsers = () => {
+  return (
+    <Card
+      sx={{ maxWidth: 400, minHeight: 400, maxHeight: 400, overflow: "auto" }}
+      className="flex flex-col justify-start"
+    >
+      <AppBar position="sticky" color="default">
+        <CardHeader title={"Github Users"} className="text-center" />
+        <Toolbar id="back-to-top-anchor" className="hidden" />
+      </AppBar>
+      {users.map((user, idx) => (
+        <User user={user} key={idx} />
+      ))}
+    </Card>
+    // https://mui.com/material-ui/
+  );
+};
+
+const User = ({ user }: any) => {
+  const { id, name, jiraTicket } = user;
+  return (
+    <CardContent className="flex justify-between items-center">
+      <Typography
+        variant="h5"
+        component="div"
+        className="outline flex-grow pl-2 rounded-md outline-gray-300"
+      >
+        {name}
+        <Typography>{`Jira Ticket: ${jiraTicket} `}</Typography>
+      </Typography>
+      <Avatar
+        src={`https://avatars.githubusercontent.com/u/${id}`}
+        className="ml-6"
+        sx={{ width: 56, height: 56 }}
+      />
+    </CardContent>
+  );
+};
+
+export default GithubUsers;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 
+import GithubUsers from "./components/GithubUsers";
 import Chart from "./components/PieChart";
 
 export default function Home() {
   return (
     <>
-      <div className="flex">
+    {/* https://echarts.apache.org/examples/en/editor.html?c=treemap-sunburst-transition */}
+      {/* <div className="flex">
         <Chart chartData={[{ testType: 'Features', failures: 25, passes: 75 }]} />
         <Chart chartData={[{ testType: 'Scenarios', failures: 50, passes: 50 }]} />
         <Chart chartData={[{ testType: 'Steps', failures: 37, passes: 63 }]} />
-      </div>
+      </div> */}
+      <GithubUsers />
     </>
   );
 }
