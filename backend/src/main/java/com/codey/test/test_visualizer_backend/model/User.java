1c50e9419ef5f94047bff6a730b86b1ef6e0aea9
Codey Gallup <codey.gallup@gmail.com>
2025-06-09 07:53:08 -0600

    User model update

--- backend/src/main/java/com/codey/test/test_visualizer_backend/model/User.java
+++ backend/src/main/java/com/codey/test/test_visualizer_backend/model/User.java
@@ -1,8 +1,17 @@
 package com.codey.test.test_visualizer_backend.model;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,24 +20,25 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String username;
+  @Column(nullable = false, unique = true)
+  private String username;
 
-    @Column(nullable = false)
-    private String password;
+  @Column(nullable = false)
+  private String password;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+  @Column(nullable = false, unique = true)
+  private String email;
 
-    private String fullName;
+  private String fullName;
 
-    @Column(name = "is_active")
-    private boolean active = true;
+  @Column(name = "is_active")
+  private boolean active = true;
 
-    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
-    private Set<Feature> features = new HashSet<>();
+  @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+  @JsonIgnore
+  private Set<Feature> features = new HashSet<>();
 }
